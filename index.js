8ac3d336ca827481affdfbf80d4bde950f00a963
mwoolf87 <michael.j.woolf@gmail.com>
2021-08-21 14:00:50 -0400

    called team API and loaded teams into dropdown

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-// const data = null;
-
-// const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === this.DONE) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open(
-//   "GET",
-//   "https://odds.p.rapidapi.com/v1/odds?sport=soccer_epl&region=uk&mkt=h2h&dateFormat=iso&oddsFormat=decimal"
-// );
-// xhr.setRequestHeader(
-//   "x-rapidapi-key",
-//   "4058387ef9msh153247a8cc0ae8bp114b7ejsn04ba7f02c7ad"
-// );
-// xhr.setRequestHeader("x-rapidapi-host", "odds.p.rapidapi.com");
-
-// xhr.send(data);
-
 console.log("sanity check");
+
+fetch("https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2019", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    "x-rapidapi-key": "ce6a7de74fmshdd457dee571ad31p15544ejsnf2f72efbc613"
+  }
+})
+  .then(response => {
+    return response.json();
+  })
+  .then(response => {
+    console.log(response);
+    displayTeamData(response);
+  });
 
 loadBetData = () => {
   fetch(
@@ -38,9 +30,41 @@ loadBetData = () => {
       return response.json();
     })
     .then(betResponse => {
-      console.log(betResponse);
+      console.log(betResponse.data);
       //   loadBetData(betResponse, "fetchid");
     });
 };
 
 loadBetData();
+
+function displayTeamData(response) {
+  const teams = response.response;
+  let teamDropDown = document.getElementById("teamOptions");
+  for (let i = 0; i < teams.length; i++) {
+    let teamOptions = teams[i].team.name;
+    let el = document.createElement("option");
+    el.value = teams[i].team.id;
+    el.innerText = teamOptions;
+    teamDropDown.appendChild(el);
+  }
+  let result = teamDropDown.value;
+  teamDropDown.addEventListener("click", e => {
+    let teamId = e.target.value;
+    fetch(
+      `https://api-football-v1.p.rapidapi.com/v3/teams?id=${teamId}&league=39&season=2019`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+          "x-rapidapi-key": "ce6a7de74fmshdd457dee571ad31p15544ejsnf2f72efbc613"
+        }
+      }
+    )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+}
